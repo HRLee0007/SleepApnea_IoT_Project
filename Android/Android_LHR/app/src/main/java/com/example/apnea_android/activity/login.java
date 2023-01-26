19556ae877cc56d1505b01925282810d6d75229a
kdozlo <eodud5730@naver.com>
2023-01-26 15:30:56 +0900

    메인 페이지 구현중

--- Android/Android_LHR/app/src/main/java/com/example/apnea_android/activity/login.java
+++ Android/Android_LHR/app/src/main/java/com/example/apnea_android/activity/login.java
@@ -11,6 +11,6 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.measure_start_stop);
     }
 }
